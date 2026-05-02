73473293e51123dcb688fee3e7531cecb17b3c21
MrFoxRookie <Zahidtejedaamaro@gmail.com>
2026-05-02 17:09:30 -0600

    Se agregaron las tarjetas para cada usuario en saved-news

--- Frontend/src/components/NewsCard/NewsCard.jsx
+++ Frontend/src/components/NewsCard/NewsCard.jsx
@@ -1,17 +1,26 @@
 function NewsCard({ card }) {
-  const { title, text, date, source, link, image, keyword } = card;
+  const {
+    article_id,
+    publishedAt,
+    source,
+    title,
+    description,
+    url,
+    urlToImage,
+  } = card;
+
+  const date = new Date(publishedAt).toLocaleDateString("es-Mx");
 
   return (
     <div className="news-card">
       <div className="news-card__image-container">
-        <img src={image} alt="" className="news-card__image" />
-        <p className="news-card__category">{keyword}</p>
+        <img src={urlToImage} alt="" className="news-card__image" />
         <button className="news-card__remove-button"></button>
       </div>
       <div className="news-card__text-container">
         <p className="news-card__date">{date}</p>
         <h2 className="news-card__title">{title}</h2>
-        <p className="news-card__text">{text}</p>
+        <p className="news-card__text">{description}</p>
         <p className="news-card__source">{source.toUpperCase()}</p>
       </div>
     </div>
--- Frontend/src/components/NewsCardList/NewsCardList.jsx
+++ Frontend/src/components/NewsCardList/NewsCardList.jsx
@@ -1,13 +1,32 @@
 import NewsCard from "../NewsCard/NewsCard";
-import cards from "../../utils/datosPrueba";
+// import cards from "../../utils/datosPrueba";
+
+import { handleGetSavedArticles } from "../../utils/api/getArticles";
+import { useEffect, useState } from "react";
 
 function NewsCardList() {
+  const [savedArticles, setSavedArticles] = useState(null);
+
+  useEffect(() => {
+    handleGetSavedArticles()
+      .then((articles) => {
+        setSavedArticles(articles);
+      })
+      .catch((err) => {
+        console.error("Error al obtener artículos:", err);
+      });
+  }, []);
+
   return (
     <section className="card-list">
       <div className="card-list__container">
-        {cards.map((card) => (
-          <NewsCard key={card._id} card={card} />
-        ))}
+        {(savedArticles || []).map(
+          (
+            card, //Asi la aplicacion no truena ya que al menos siempre habra un array vacio.
+          ) => (
+            <NewsCard key={card.article_id} card={card} />
+          ),
+        )}
       </div>
     </section>
   );

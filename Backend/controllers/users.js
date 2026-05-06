35217437d505b38132945fd1586cbcdeed75b9d7
MrFoxRookie <Zahidtejedaamaro@gmail.com>
2026-05-06 13:06:06 -0600

    Cambio del catch de signup

--- Backend/controllers/users.js
+++ Backend/controllers/users.js
@@ -9,14 +9,15 @@ export class UserController {
     try {
       const result = validateSignup(req.body);
       if (!result.success) {
-        return res.status(400).json({ error: result.error.issues }); //en result.error.issues estan los errores que arroja el schema
+        return res.status(400).json({ error: result.error.issues });
       }
 
       const newUser = await UserModel.signup({ input: result.data });
 
       res.status(201).json(newUser);
-    } catch (error) {
-      res.status(400).json({ error: error.message }); // Este es el error capturado desde el Model o desde cualquier parte del código. error.message contiene el mensaje del error lanzado o generado por Node.
+    } catch (err) {
+      console.log("ERROR SIGNUP:", err);
+      return res.status(400).json({ error: err.message });
     }
   }
 
--- Backend/routes/users.js
+++ Backend/routes/users.js
@@ -7,8 +7,7 @@ import { auth } from "../middlewares/auth.js";
 export const usersRouter = Router();
 
 usersRouter.post("/signup", UserController.signup);
-// usersRouter.post("/", UserController.signin);
 
 usersRouter.post("/signin", UserController.signin);
 
-usersRouter.get("/me", auth, UserController.getCurrentUser); //El id esta dentro del token y, al pasar por el auth, esto hace que ahora el id se pueda extraer del req del usuario
+usersRouter.get("/me", auth, UserController.getCurrentUser);

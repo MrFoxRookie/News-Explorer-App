bf499901f8215f044a07bf24ef18d18d161ee1ba
MrFoxRookie <Zahidtejedaamaro@gmail.com>
2026-05-07 10:24:43 -0600

    Se agrego dinamsimo a las env

--- Backend/database/connectionMySQL.js
+++ Backend/database/connectionMySQL.js
@@ -1,5 +1,10 @@
 import mysql from "mysql2/promise";
 import dotenv from "dotenv";
-dotenv.config();
+dotenv.config({
+  path:
+    process.env.NODE_ENV === "production"
+      ? ".env.production"
+      : ".env.development",
+});
 
 export const pool = mysql.createPool(process.env.DATABASE_URL);
--- Backend/middlewares/cors.js
+++ Backend/middlewares/cors.js
@@ -1,21 +1,15 @@
 // Ese código usa el paquete cors, que NO viene por defecto.
 
-// 👉 Debes instalarlo:
-
 // npm install cors
-// 🧠 ¿Qué hace ese código?
 
 // Es un middleware de CORS personalizado.
 
-// 👉 Controla qué dominios pueden hacer requests a tu backend
+//Controla qué dominios pueden hacer requests a tu backend
 
 import cors from "cors";
 
 const ACCEPTED_ORIGINS = [
-  "http://localhost:8080",
-  "http://localhost:1234",
-  "https://movies.com",
-  "https://midu.dev",
+  "http://localhost:1234, https://news-explorer-app-three.vercel.app/",
 ];
 
 export const corsMiddleware = ({ acceptedOrigins = ACCEPTED_ORIGINS } = {}) =>
